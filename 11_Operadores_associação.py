861bfee40d09d8e55bad7740ad6dfd9bd891fef5
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-02-27 12:00:52 -0300

    Aula de operação de associação

---
+++ 11_Operadores_associação.py
@@ -0,0 +1,10 @@
+# par saber se aquele valor está dentro do outro (listas, vetores etc)
+# comando é IN para saber se está dentro
+# comando NOT IN para saber se não está dentro
+
+frutas = ["maça", "banana", "limão"]
+
+print("laranja" in frutas)
+print("maça" in frutas)
+print("banana" not in frutas)
+print(25 not in frutas) 
