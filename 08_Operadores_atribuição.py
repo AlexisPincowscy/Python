1982e265fc8d152be8408d367527fd2a56e750b7
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-02-27 11:23:52 -0300

    Aula operador de atribuição

---
+++ 08_Operadores_atribuição.py
@@ -0,0 +1,22 @@
+saldo = 500
+
+saldo += 100
+print(saldo)
+
+saldo -= 50
+print(saldo)
+
+saldo *= 10
+print(saldo)
+
+saldo //= 3
+print(saldo)
+
+saldo /= 5
+print(saldo)
+
+saldo **= 2
+print(saldo)
+
+saldo %= 5
+print(saldo)
