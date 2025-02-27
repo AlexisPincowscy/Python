f868442559c5f452d4abaad91944fe7014c7ad2a
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-02-27 11:08:42 -0300

    Aula de operadores comparativos

---
+++ 07_Operadores_comparação.py
@@ -0,0 +1,15 @@
+saldo = 400
+saque = 200
+
+# igual
+print(saldo == saque) 
+# diferente
+print(saldo != saque)
+# maior
+print(saldo > saque)
+# maior ou igual à
+print(saldo >= saque)
+# menor
+print(saldo < saque)
+# menor ou igual à
+print(saldo <= saque)
