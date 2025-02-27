5318e61509c6cd87066e2c0cba84904478b62ec0
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-02-27 10:55:15 -0300

    Aula operadores aritiméticos

---
+++ 06_Operadores_aritimeticos.py
@@ -0,0 +1,27 @@
+produto_1 = 10
+produto_2 = 20
+
+# soma
+print(produto_1 + produto_2)
+
+# subtração
+print(produto_1 - produto_2)
+
+# multiplicação
+print(produto_1 * produto_2)
+
+# divisão normal
+print(produto_1 / produto_2)
+# divisão inteira
+print(produto_1 // produto_2)
+
+# potência
+print(produto_1 ** 10)
+
+# resto da divisão
+print(produto_1 % produto_2)
+
+# em expressões seguir a regra matemática de pesos
+# parenteses, potências, multi e div (da esquerda -> direita) e som e sub (esquera -> direita)
+x =  (10 + 15) * 4**3 - 25
+print(x)
