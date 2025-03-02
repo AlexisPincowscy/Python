8de05bfbe1382c58f5da29f2bd04d042282c96a6
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-02 20:14:47 -0300

    Aula interpolação variável

---
+++ 15_Interpolaçao_variáveis.py
@@ -0,0 +1,9 @@
+# a função F string serve para interpolar (misturar) tipos de dados na string.
+
+nome = "Alex"
+idade = 36
+profissao = "estudante"
+
+print(f"{nome} tem {idade} anos e trabalha na função de {profissao}")
+
+# existem outrso métodos, que são antigos como % e .format, mas já não são utilizados
