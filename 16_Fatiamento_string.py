0d4b81856e966ff4e7cffc3c744df3cb9857f5ab
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-02 20:26:14 -0300

    Aula de fatiamento de string

---
+++ 16_Fatiamento_string.py
@@ -0,0 +1,10 @@
+# Como selecionar o caracter de uma string
+
+nome = "Alexis Pincowscy"
+
+print(nome[0]) #aqui ele pega o primerio caracter
+print(nome[:9]) #aqui ele vai pegar até o nono caracter
+print(nome[13:]) #ele pega a partir do 12 item para frente
+print(nome[5:10]) #começa do 4 e vai até o 9 caracter
+print(nome[5:20:2]) #pega no intervalo e de 2 em 2 caracter
+print(nome[:: -1])#vai pegar a string invertida
