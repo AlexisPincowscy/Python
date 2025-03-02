09a144e89de041f02e162270ecd5d62f24bed5a4
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-02 20:34:03 -0300

    Aula de string multilinhas

---
+++ 17_String_multilinhas.py
@@ -0,0 +1,15 @@
+# string de multipals linhas ou triplas
+# útil para mensagem grande, como menus, ela aceita e mantem os espaços de indentação
+#ele imprimi exatamente como a mensagem está
+
+print('''
+
+    *****MENU****
+      
+        [1] Comprar
+        [2] Vender
+        [3] Sair
+      
+    **OBRIGADO**
+      
+''')
