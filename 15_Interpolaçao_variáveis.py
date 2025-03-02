8b86e3ddbd15dc1a4de0b82a669d32c3d6800b9b
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-02 20:20:00 -0300

    Acrescentando funçã de casas decimais

--- 15_Interpolaçao_variáveis.py
+++ 15_Interpolaçao_variáveis.py
@@ -3,7 +3,9 @@
 nome = "Alex"
 idade = 36
 profissao = "estudante"
+altura = 1.8023
 
 print(f"{nome} tem {idade} anos e trabalha na função de {profissao}")
-
+print(f"{nome} tem {altura:.2f} de altura") #exemplo para colcoar 2 casa decimais
+print(f"{nome} tem {altura:10.2f} de altura")#o primeiro nome antes do .2f seria a quantidade de casas a esquerda
 # existem outrso métodos, que são antigos como % e .format, mas já não são utilizados
